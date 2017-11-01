9c626c2e1334f7f9ffed7debe2828ba9557fb94a
Andrew Minion <andrew@andrewrminion.com>
2017-10-31 21:37:10 -0400

    update parent theme “continue reading” link

--- functions.php
+++ functions.php
@@ -1,1 +1,9 @@
 <?php
+
+/**
+ * Returns a "Continue Reading" link for excerpts
+ */
+function hsmom_continue_reading() {
+    return '&hellip;';
+}
+add_filter( 'excerpt_more', 'hsmom_continue_reading', 15 );
