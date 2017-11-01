d4d902711984045aa5ffc7265b5d8c96d7f296bb
Andrew Minion <andrew@andrewrminion.com>
2017-10-31 21:26:19 -0400

    add author meta to single.php

--- single.php
+++ single.php
@@ -25,9 +25,17 @@ get_header(); ?>
         <?php get_template_part( 'navigation', 'single' ); ?>
 
         <?php if ( get_the_author_meta( 'description' ) ) : ?>
+        <?php
+        $author_id = get_the_author_meta( 'ID' );
+        $author_url = get_field( 'user_ghc_speaker_page', 'user_' . $author_id );
+        $author_image = get_field( 'user_profile_photo', 'user_' . $author_id );
+        $promo_url = get_field( 'user_promo_url', 'user_' . $author_id );
+        $promo_image = get_field( 'user_promo_image', 'user_' . $author_id );
+        ?>
+
         <div class="author-box">
             <div class="author-img">
-                <?php echo get_avatar( get_the_author_meta( 'user_email' ), '100' ); ?>
+                <?php echo wp_get_attachment_image( $author_image, array( 100, 100 ), false, array( 'class' => 'avatar avatar-100 photo' ) ) ?>
             </div>
             <h4 class="author-name">
                 <?php the_author_meta( 'display_name' ); ?>
@@ -35,6 +43,24 @@ get_header(); ?>
             <p class="author-description">
                 <?php the_author_meta( 'description' ); ?>
             </p>
+
+            <?php
+            if ( $author_url ) {
+                echo '<p><a href="' . $author_url . '">More about ' . get_the_author() . ' &rarr;</a></p>';
+            }
+
+            if ( $promo_url ) {
+                echo '<a href="' . $promo_url . '" target="_blank" rel="noopener">';
+            }
+            if ( $promo_image ) {
+                echo wp_get_attachment_image( $promo_image, 'full', false, array( 'class' => 'promo-image' ) );
+            }
+            if ( $promo_url ) {
+                echo '</a>';
+            }
+
+            ?>
+
         </div>
         <?php endif; ?>
 
